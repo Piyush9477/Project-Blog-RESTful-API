d9fe8973567457168a800a63fe33a314355f5155
Piyush Agarwal <piyushag7777@gmail.com>
2024-07-07 11:01:54 +0530

    create user model and signup route

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
 dotenv.config();
 const connetMongoDb = require("./init/mongodb");
+const {authRoute} = require("./routes");     
 
 //init app
 const app = express();
@@ -13,5 +14,8 @@ connetMongoDb();
 //third-party middleware
 app.use(express.json({limit:"500mb"}));
 app.use(bodyParser.urlencoded({limit:"500mb", extended:true}));
+
+//route section
+app.use("/api/v1/auth", authRoute);
 
 module.exports = app;
