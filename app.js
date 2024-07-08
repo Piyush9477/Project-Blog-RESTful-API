e8b8f3b177d3bfc7d28554af2a4cfe7b5b95698c
Piyush Agarwal <piyushag7777@gmail.com>
2024-07-08 10:31:06 +0530

    add error handling middlewares

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const dotenv = require("dotenv");
+const morgan = require("morgan");
 dotenv.config();
 const connetMongoDb = require("./init/mongodb");
 const {authRoute} = require("./routes");     
+const {errorHandler} = require("./middlewares");
+const notFound = require("./controllers/notFound");
 
 //init app
 const app = express();
@@ -14,8 +17,15 @@ connetMongoDb();
 //third-party middleware
 app.use(express.json({limit:"500mb"}));
 app.use(bodyParser.urlencoded({limit:"500mb", extended:true}));
+app.use(morgan("dev"));
 
 //route section
 app.use("/api/v1/auth", authRoute);
+
+//not found route
+app.use("*",notFound);
+
+//error handling middleware
+app.use(errorHandler);
 
 module.exports = app;
--- controllers/auth.js
+++ controllers/auth.js
@@ -4,11 +4,31 @@ const signup = async (req, res, next) => {
     try{
         const {name, email, password, role} = req.body;
 
+        if(!name){
+            res.code = 400;
+            throw new Error("Name is required");
+        }
+
+        if(!email){
+            res.code = 400;
+            throw new Error("email is required");
+        }
+
+        if(!password){
+            res.code = 400;
+            throw new Error("Password is required");
+        }
+
+        if(password.length < 6){
+            res.code = 400;
+            throw new Error("Minimum length of password should be 6");
+        }
+
         const newUser = User({name, email, password, role});
 
         await newUser.save();
 
-        res.status(201).json({message: "User registered successfully"});
+        res.status(201).json({code: 201, status: true, message: "User registered successfully"});
     }catch(error){
         next(error);
     }
